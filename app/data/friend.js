0a7625fe8b2d5d05d8db73137ee996bc3464ee9f
mriddle86 <marshall.riddle@gmail.com>
2017-11-16 23:14:57 -0800

    almost done

---
+++ app/data/friend.js
@@ -0,0 +1,116 @@
+var friends = [
+  {
+      "name":"Hank",
+      "photo":"https://i.pinimg.com/736x/1e/d1/a6/1ed1a6fa62526e1667162c3368b362bd--mike-judge-funny-pranks.jpg",
+      "scores":[
+         5,
+         5,
+         5,
+         5,
+         5,
+         5,
+         5,
+         5,
+         5,
+         5
+          ]
+      },
+  {
+  "name":"Bobby",
+  "photo":"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/hostedimages/1386391767i/7354321.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+      ]
+},
+{
+  "name":"Peggy",
+  "photo":"https://scontent.cdninstagram.com/t51.2885-19/11254444_667496046714522_2021776729_a.jpg",
+  "scores":[
+     3,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+      ]
+},
+{
+  "name":"Luanne",
+  "photo":"https://vignette.wikia.nocookie.net/kingofthehill/images/4/47/Luanne.jpg/revision/latest?cb=20151004012746",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     4,
+     2,
+     1,
+     2,
+     1
+      ]
+},
+{
+  "name":"Dale",
+  "photo":"https://scontent-sea1-1.cdninstagram.com/t51.2885-19/s150x150/14288159_1582177812088518_1880310558_a.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     5,
+     3,
+     4,
+     4,
+     5
+      ]
+},
+{
+  "name":"Bill",
+  "photo":"https://i.imgflip.com/2/1kqlbf.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     1,
+     3,
+     4,
+     5,
+     5
+      ]
+},
+{
+  "name":"boomhauer",
+  "photo":"https://scontent-ams3-1.cdninstagram.com/t51.2885-19/s150x150/18579761_116220242288884_5551105081002164224_a.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     4,
+     4,
+     5,
+     5,
+     5
+      ]
+},
+]
+
+module.exports = friends;
